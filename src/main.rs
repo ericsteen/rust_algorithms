78db3f94b8d3e5401364f0b5e488c9d014a7c0d8
Eric Steen <ericsteen1@gmail.com>
2020-07-24 16:56:50 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Multiplying Matrices
+extern crate ndarray;
+use ndarray::arr2;
+
 fn main() {
-    println!("Hello, world!");
+    let a = arr2(&[[1, 2, 3],
+                   [4, 5, 6]]);
+
+    let b = arr2(&[[6, 3],
+                   [5, 2],
+                   [4, 1]]);
+
+    println!("{}", a.dot(&b));
 }
